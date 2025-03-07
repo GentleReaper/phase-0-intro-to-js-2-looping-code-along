1755d3e1fc092b77b3d6691f0ea3fef32e452b8d
GentleReaper <cyraxmuia@gmail.com>
2025-03-07 21:24:14 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// index.js
+names= ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i <names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
+    
